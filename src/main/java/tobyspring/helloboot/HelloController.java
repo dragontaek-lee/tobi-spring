39d787718ee00689648b3e3890964ca86f3e7f57
dragontaek-lee <dragontaek98@naver.com>
2023-03-06 14:08:15 +0900

    feat: controller practice

---
+++ src/main/java/tobyspring/helloboot/HelloController.java
@@ -0,0 +1,12 @@
+package tobyspring.helloboot;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+    public String hello(String name) {
+        return "Hello " + name;
+    }
+}
