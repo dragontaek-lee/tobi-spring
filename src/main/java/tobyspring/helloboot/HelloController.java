a50319a8c6bfeacf0958e3ac61a2b16377767c2b
dragontaek-lee <dragontaek98@naver.com>
2023-03-08 17:54:17 +0900

    feat: hello controller object

--- src/main/java/tobyspring/helloboot/HelloController.java
+++ src/main/java/tobyspring/helloboot/HelloController.java
@@ -1,11 +1,6 @@
 package tobyspring.helloboot;
 
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.GetMapping;
-
-@RestController
 public class HelloController {
-    @GetMapping("/hello")
     public String hello(String name) {
         return "Hello " + name;
     }
